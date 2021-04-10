4579a5b2cd97d011161f7b760eaafda42c04a1fc
Sarika Kashyap <70009069+sarikakashyap@users.noreply.github.com>
2021-04-11 00:45:17 +0530

    Create display_text.py

---
+++ simple_CGI/display_text.py
@@ -0,0 +1,11 @@
+#!C:/Python/Python37/python.exe
+import cgi
+print("Content-type:text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>Simple code</title>")
+print("</head>")
+print("<body>")
+print("<h1>Simple Display Text</h1>")
+print("</body>")
+print("</html>")
